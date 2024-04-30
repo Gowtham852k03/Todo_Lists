b378897b6eb77e611fd93f384dee873161c31332
Your Name <your_email@example.com>
2024-04-30 22:28:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,50 @@
-import React from "react"
+import React, { useEffect } from "react";
 import { useState } from "react";
 import Header from "./Header";
 import AddItem from "./AddItem";
 import Content from "./Content";
-import "./index.css"
+import "./index.css";
 import Footer from "./Footer";
+
 function App() {
-
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('todolist'))
-
-  );
+  const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState('');
+
+  useEffect(() => {
+
+    const storedItems = JSON.parse(localStorage.getItem("todolist"));
+    if (storedItems) {
+      setItems(storedItems);
+    }
+  }, []);
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const addNewItem = { id, checked: false, item };
     const listItems = [...items, addNewItem];
     setItems(listItems);
-    localStorage.setItem("todolist", JSON.stringify(listItems))
-  }
+    localStorage.setItem("todolist", JSON.stringify(listItems));
+  };
+
   const handleCheck = (id) => {
-    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item)
-    setItems(listItems)
-    localStorage.setItem("todolist", JSON.stringify(listItems))
-  }
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setItems(listItems);
+    localStorage.setItem("todolist", JSON.stringify(listItems));
+  };
+
   const deleteItem = (id) => {
-    const listItems = items.filter((item) => item.id !== id)
-    setItems(listItems)
-    localStorage.setItem("todolist", JSON.stringify(listItems))
-  }
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("todolist", JSON.stringify(listItems));
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!newItem) return;
     addItem(newItem);
     setNewItem('');
-  }
+  };
+
   return (
     <div className="App">
       <Header />
@@ -48,9 +58,8 @@ function App() {
         handleCheck={handleCheck}
         deleteItem={deleteItem}
       />
-      <Footer length={(items.length)} />
+      <Footer length={items.length} />
     </div>
-
   );
 }
 
